88856cf61bf6f27928e1d3c96702b83a95c23122
oleksiiseleznov <alexey.seleznov@gmail.com>
2018-05-16 15:09:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -42,22 +42,7 @@ mkdir -p /var/www/
 
 CONFFILE=/var/www/reviewboard/conf/settings_local.py
 
-#if [[ ! -d /var/www/reviewboard ]]; then
-#    rb-site install --noinput \
-#        --domain-name="$DOMAIN" \
-#        --site-root="$SITE_ROOT" \
-#        --static-url=static/ --media-url=media/ \
-#        --db-type=postgresql \
-#        --db-name="$PGDB" \
-#        --db-host="$PGHOST" \
-#        --db-user="$PGUSER" \
-#        --db-pass="$PGPASSWORD" \
-#        --cache-type=memcached --cache-info="$MEMCACHED" \
-#        --web-server-type=lighttpd --web-server-port=8000 \
-#        --admin-user=admin --admin-password=admin --admin-email=admin@example.com \
-#        /var/www/reviewboard/
-#fi
-
+#Installation script of web-site 
 rb-site install --noinput --domain-name=* --site-root=/ --media-url=/media/ --db-type=postgresql --db-host=rb-postgres --db-name=reviewboard --db-user=reviewboard --db-pass=reviewboard --cache-type=memcached --cache-info=rb-memcached:11211 --admin-user=admin --admin-password=12345Qwert --admin-email=test@test.com /var/www/reviewboard
 
 if [[ "${DEBUG}" ]]; then
